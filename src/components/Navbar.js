1e93b4c5d16b2e733c35be0de27fafdc666b302f
Yimiao Zhang <yimiaoz@mun.ca>
2020-12-31 19:16:46 -0330

    Update navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import logo  from "../logo.png";
+// REACT Fontawesome fonts//
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+export const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <div className="container">
+        
+   
+                <a className="navbar-brand" href="#"><img src = {logo} alt = "logo..." /></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <FontAwesomeIcon icon = {faBars} style = {{color: "#fff"}}/>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">about me</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Resume</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Project</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contact</a>
+                    </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
