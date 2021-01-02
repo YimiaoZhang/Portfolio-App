c68d7a212b78b3705f2a083ffd43fc4b50aed8aa
Yimiao Zhang <yimiaoz@mun.ca>
2021-01-02 13:32:47 -0330

    Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import Typed from 'react-typed';
-const Header = () => {
-    return (
-        <div className = "header-wraper">
-            <div className = "main-info">
-                <h1>web development and websites pr</h1>
-                <Typed 
-                    className = "typed-text"
-                    strings = {["Web Design", "Web Development", "Facebook Ads SMM", "Google Ads"]}
-                    typeSpeed = {40}
-                    backSpeed = {60}
-                    loop
-                />
-                <a href="#" className = "btn-main-offer">Contact Me</a>
-            </div>
+import React from "react";
+import Typed from "react-typed";
+import { Link } from "react-scroll";
 
-        </div>
-    )
+
+const Header = () => {
+  return (
+    <div id="home" className="header-wraper">
+      <div className="main-info">
+        <canvas></canvas>
+        <h1>web development and websites promotions</h1>
+        <Typed
+          className="typed-text"
+          strings={["Web Design", "Web Development", "Facebook Ads SMM", "Google Ads"]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <Link smooth={true} to="contacts" offset={-110} className="btn-main-offer">contact me</Link>
+      </div>
+    </div>
+  )
 }
 
-export default Header
+export default Header;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,49 @@
-import React from 'react';
-import logo  from "../logo.png";
-// REACT Fontawesome fonts//
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-export const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-            <div className="container">
-        
-   
-                <a className="navbar-brand" href="#"><img src = {logo} alt = "logo..." /></a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <FontAwesomeIcon icon = {faBars} style = {{color: "#fff"}}/>
-                </button>
+import React from "react";
+import logo from "../logo.png";
+import { Link } from "react-scroll";
+// REACT FONTAWESOME IMPORTS
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">about me</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Resume</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Project</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Contact</a>
-                    </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
+      <div className="container">
+
+        <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..." /></a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <Link smooth={true} to="home" className="nav-link" href="#">Home <span className="sr-only">(current)</span></Link>
+            </li>
+            <li className="nav-item">
+              <Link smooth={true} to="about" offset={-110} className="nav-link" >about me</Link>
+            </li>
+            <li className="nav-item">
+              <Link smooth={true} to="services" offset={-110} className="nav-link" href="#">services</Link>
+            </li>
+            <li className="nav-item">
+              <Link smooth={true} to="experience" offset={-110} className="nav-link" >experience</Link>
+            </li>
+            <li className="nav-item">
+              <Link smooth={true} to="portfolio" offset={-110} className="nav-link" >portfolio</Link>
+            </li>
+
+            <li className="nav-item">
+              <Link smooth={true} to="contacts" offset={-110} className="nav-link" >contacts</Link>
+            </li>
+
+          </ul>
+
+        </div>
+
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
